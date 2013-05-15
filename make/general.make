268c6b64c0cc28283970574ba510af6eb254ea15
Daniel Oertwig <Daniel.Oertwig@googlemail.com>
2013-05-15 04:06:38 +0200

    Added Makefile include with general definitions

---
+++ make/general.make
@@ -0,0 +1,8 @@
+# General variables used all over the place
+
+AR := avr32-ar
+AS := avr32-as
+CC := avr32-gcc
+LD := avr32-ld
+CPP := avr32-cpp
+CXX := avr32-cxx
--- make/toplevel.mk
+++ make/toplevel.mk
@@ -4,7 +4,7 @@
 # * Handles all target
 # * Place to include submakefiles
 
-#include make/general.mk
+include make/general.mk
 #include make/asf.mk
 #include make/autoprereq.mk
 
